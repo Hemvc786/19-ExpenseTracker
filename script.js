58eba61879a4107de646a6b0d56d0e5f99f6f11a
Hemant Chaudhari <hemantvc786@gmail.com>
2025-05-01 08:38:44 +0530

    Added the functionality to expense tracker app....

--- script.js
+++ script.js
@@ -1,1 +1,95 @@
- 
+const balance = document.getElementById('balance');
+const money_plus = document.getElementById('money-plus');
+const money_minus = document.getElementById('money-minus');
+const list = document.getElementById('list');
+const form = document.getElementById('form');
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
+
+//JSON string->JS obj
+const localStorageTransactions = JSON.parse(
+  localStorage.getItem('transactions')
+);
+
+let transactions =
+  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+
+function addTransaction(e) {
+  e.preventDefault(); //cancels the event if it is cancelable
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please Add a Text or an Amount');
+  } else {
+    //obj
+    const transaction = {
+      id: generateID(), //id generation
+      text: text.value,
+      amount: +amount.value,
+    };
+    transactions.push(transaction);
+    addTransactionDOM(transaction);
+    updateValues();
+    updateLocalStorage();
+
+    //Setting empty strigs @end
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+function generateID() {
+  return Math.floor(Math.random() * 1000000);
+}
+
+function addTransactionDOM(transaction) {
+  //display things for uuuuu
+  const sign = transaction.amount < 0 ? '-' : '+';
+  const item = document.createElement('li');
+  item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+  item.innerHTML = ` 
+  ${transaction.text}
+   <span>${sign} ${Math.abs(transaction.amount)}</span>
+   <button class='delete-btn' onClick="removeTransaction(${
+     transaction.id
+   })">X</button>
+   `;
+  list.appendChild(item);
+}
+
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
+  balance.innerHTML = `$${total}`;
+  money_plus.innerHTML = `$${income}`;
+  money_minus.innerHTML = `$${expense}`;
+}
+
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+  updateLocalStorage();
+  init();
+}
+
+function updateLocalStorage() {
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function init() {
+  list.innerHTML = '';
+  transactions.forEach(addTransactionDOM);
+  updateValues();
+}
+
+init();
+
+form.addEventListener('submit', addTransaction);
